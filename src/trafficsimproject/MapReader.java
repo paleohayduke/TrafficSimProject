1ba75666803e8516d428c58d100e29401e468023
paleo <paleo@DESKTOP-33OL0F1>
2018-10-12 09:08:08 -0500

    Merge origin/master
    Conflicts:
    	README.md

--- src/trafficsimproject/MapReader.java
+++ src/trafficsimproject/MapReader.java
@@ -202,7 +202,7 @@ public class MapReader {
                             tempRoad.numLanes=2;
                         }else if(roadType.equals("residential")||roadType.equals("residential_link")){
                             tempRoad.speed=.00016;
-                            tempRoad.numLanes=2;
+                            tempRoad.numLanes=1;
                         }
                     }
                 }
